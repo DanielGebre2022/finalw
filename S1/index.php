f546b09570cf766aa2be8555901bd4615d7c5ca8
DanielGebre2022 <91226945+DanielGebre2022@users.noreply.github.com>
2022-03-25 19:54:29 -0700

    Add files via upload

---
+++ S1/index.php
@@ -0,0 +1,57 @@
+<?php include 'includes/header.php';?>
+<div class="container">
+    <div class="collage">
+        <div class="item item-1">
+            <img class="chicken" src="./images/chicken.jpg"></img>
+        </div>
+        <div class="item item-2">
+            <img class="lasagna" src="./images/lasagna.jpg"></img>
+        </div>
+        <div class="item item-3">
+            <img class="nachos" src="./images/nachos.jpg"></img>
+        </div>
+        <div class="item item-4">
+            <img class="shortRibs" src="../images/gb_2005-110.jpeg"></img>
+        </div>
+        <div class="item item-5">
+            <h3 class="item-5-text">EXPERIENCE</h3>
+            <h3 class="item-5-text" id='S1'>S1</h3>
+            <button type="button" class="btn btn-light experience">EXPLORE</button>
+        </div>
+    </div>
+    <div class="underMain">
+        <div class="underMainPic">
+            <img class="boxLunchPic" src="./images/box-lunch.jpg"></img>
+        </div>
+        <div class="underMainText">
+            <h2 class="underMainTitle">Premier Catering</h2>
+            <p class="underMainP">Lorem ipsum dolor sit amet. Et aliquid molestias hic dignissimos obcaecati est optio assumenda? Nam delectus nihil ea molestiae magni et obcaecati molestiae est ipsum dolor qui officia quia ad pariatur porro. Quo quisquam esse qui minima voluptas est iusto vero. Et ratione delectus eum nemo doloribus et labore laborum ea amet blanditiis! Id porro error est Quis dicta eum sint sequi. Nam blanditiis dolor aut sapiente mollitia ut reprehenderit assumenda. Ex internos quia ut aspernatur quisquam qui molestiae voluptate ut pariatur adipisci At libero amet vel magni inventore. Sit quasi mollitia eos ullam minus qui architecto adipisci. Sed tempore nobis qui dolores sunt est quam dolores aut rerum deserunt et aspernatur molestiae quo quam iste.</p>
+        </div>
+
+    </div>
+    <div class="threeCards">
+        <div class="blueCard card">
+            <h4 class="blueCardTitle">Delicous Food</h4>
+            <p class="blueCardText">Lorem ipsum dolor sit amet. Ad dolorem doloribus 33 soluta laborum vel natus tempora et repudiandae nulla. Ab assumenda quia hic i </p>
+        </div>
+        <div class="orangeCard card">
+            <h4 class="orangeCardTitle"> Beautiful Presentation</h4>
+            <p class="orangeCardText">Lorem ipsum dolor sit amet. Ad dolorem doloribus 33 soluta laborum vel natus tempora et repudiandae nulla. Ab as</p>
+        </div>
+        <div class="pinkCard card">
+            <h4 class="pinkCardTitle"> Wonderful Experience</h4>
+            <p class="pinkCardText">Lorem ipsum dolor sit amet. Ad dolorem doloribus 33 soluta laborum vel natus tempora et repudiandae nulla. Ab assumenda quia hic ipsum quia et earum tempore e</p>
+        </div>
+    </div>
+    <div class="underCard">
+        <img class="bottomImage"  src="./images/enhanceschick.jpg"></img>
+    </div>
+    <div class="description">
+        <h1 class="descriptionTitle">Our Catering Services</h1>
+        <p class="descriptionText">Lorem ipsum dolor sit amet. Quia impedit est praesentium dolores 33 dolorem temporibus ut modi magni. Deserunt perferendis qui saepe ducimus et reiciendis ipsa ut deleniti omnis. Et debitis reiciendis et tempore delectus et Quis Quis quo debitis doloremque et nemo dicta qui quia quia qui dolor amet. Quo veritatis quasi in accusamus optio eum nihil similique. Non ipsum quia ex adipisci nihil est quos nulla ut dolores officia est culpa minus quo rerum maxime in officiis voluptates. Et nemo deleniti in galisum nesciunt et sunt veritatis sed exercitationem molestiae non dolorum autem sit pariatur dolore aut ipsam laborum. Hic delectus architecto aut nostrum numquam et modi atque aut quae nostrum qui quas dicta aut voluptatum dignissimos. Et quia iusto ea dolor adipisci sed inventore sapiente quo doloremque eveniet et itaque voluptatem. Est possimus enim non ipsam beatae hic voluptas molestias et quam quis eos dolores iste! Et suscipit tempore et quia consectetur est consectetur nobis qui sequi iure cum sunt consectetur. Quo facere adipisci ut mollitia mollitia ad facere placeat eum dolor facere ex sequi expedita. Eum odit velit aut quod explicabo ut molestiae sapiente. A cupiditate nobis nam amet temporibus sit rerum exercitationem sed quia rerum vel provident soluta. Nam magnam error et numquam saepe ut voluptatem excepturi nam iure rerum ea quos et voluptatem enim.</p>
+    </div>
+
+</div>
+<?php include 'includes/footer.php';?>
+</body>
+</html>
